d089fd182c186f238edb544d6f5eb3e47f780116
Stodolak <stodolak.michal@gmail.com>
2023-12-04 20:28:06 +0100

    Reorganize files

---
+++ modules/json_Operations.py
@@ -0,0 +1,58 @@
+import json
+import os
+
+
+class Json_Operations:
+    def __init__(self, category_file="default_dictionary.json"):
+        self.path = os.path.join(os.getcwd(), "dictionaries", category_file)
+        self.json_file = self.getParsedDataFromJsonFile()
+
+    def getParsedDataFromJsonFile(self):
+        try:
+            with open(self.path, "r") as file:
+                return json.load(file)
+        except FileNotFoundError:
+            with open(self.path, "w") as file:
+                json.dump({}, file, indent=2)
+                return {}
+
+    @staticmethod
+    def getWords():
+        new_word = input("Podaj słowo (\"exit\" konczy pobieranie słów): ")
+        return new_word
+
+    def updateJsonFile(self):
+        try:
+            with open(self.path, "w") as file:
+                json.dump(self.json_file, file, indent=2)
+            print("Slownik został zaktualizowany!")
+        except FileNotFoundError:
+            print(f"Plik {self.path} nie istnieje!")
+        file.close()
+
+    def getWordsToBeAdded(self):
+        while True:
+            new_word = self.getWords()
+            if new_word.lower() == "exit":
+                break
+            new_translation = input("Podaj tlumaczenie: ")
+            self.json_file[new_word] = new_translation
+
+    def getWordsToBeDeleted(self):
+        while True:
+            new_word = self.getWords()
+            if new_word.lower() == "exit":
+                break
+            if new_word in self.json_file:
+                del self.json_file[new_word]
+                print(f"Slowo {new_word} zostało usunięte!")
+            else:
+                print(f"Slowa {new_word} nie ma w slowniku!")
+
+    def addNewWordAndTranslationToJson(self):
+        self.getWordsToBeAdded()
+        self.updateJsonFile()
+
+    def deleteWordFromDictionary(self):
+        self.getWordsToBeDeleted()
+        self.updateJsonFile()
---
+++ modules/tests.py
@@ -0,0 +1,34 @@
+import random
+
+
+class Tests:
+    def __init__(self, words: dict):
+        self.words = words
+
+    def displayWords(self):
+        """Opcja 1 - Wyswietlanie zawartosci slownika"""
+        for word, translation in self.words.items():
+            print(f"{word} - {translation}")
+
+    def testKnowledge(self):
+        """Opcja 2 - Sprawdzanie wiedzy.
+        Jesli odpowiedz jest poprawna, zmniejsza się counter i slowo jest usuwane ze slownika
+        Jesli odpowiedz jest niepoprawna, powtarzamy wyswietlanie pytania
+        """
+        counter = len(self.words)
+        wrong_answers_counter = 0
+        words_copy = dict(self.words)
+
+        while counter != 0 and wrong_answers_counter <= 9:
+            random_word = random.choice(list(words_copy.keys()))
+            random_translation = words_copy[random_word]
+            user_input = input(f"{random_word} - ")
+            if user_input == f"{random_translation}":
+                print("Poprawnie!")
+                counter -= 1
+                del words_copy[random_word]
+            else:
+                print(f"Źle! Poprawnie: {random_word} - {random_translation}")
+                wrong_answers_counter += 1
+        if wrong_answers_counter == 10:
+            print("Odpowiedziałeś 10 razy źle! Zacznij do nowa!")
